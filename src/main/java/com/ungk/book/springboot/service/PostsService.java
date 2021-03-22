fdd828c9aa89287cad5d3c2ed8573ffa705c5931
Gunwoong-Heo <gjrjsdnd1129@gmail.com>
2021-03-22 18:04:14 +0900

    add findAllDesc() & delete method

--- src/main/java/com/ungk/book/springboot/service/PostsService.java
+++ src/main/java/com/ungk/book/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.ungk.book.springboot.service;
 
 import com.ungk.book.springboot.domain.posts.Posts;
 import com.ungk.book.springboot.domain.posts.PostsRepository;
+import com.ungk.book.springboot.web.dto.PostsListResponseDto;
 import com.ungk.book.springboot.web.dto.PostsResponseDto;
 import com.ungk.book.springboot.web.dto.PostsSaveRequestDto;
 import com.ungk.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,6 +35,17 @@ public class PostsService {
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 
 }
